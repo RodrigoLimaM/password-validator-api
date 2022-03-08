896b0f90e4941a6f829821acd947c16e305f7d53
RodrigoLimaM <digo100200@gmail.com>
2022-03-08 19:03:31 -0300

    initializing project

---
+++ src/main/java/br/com/passwordvalidator/PasswordValidatorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.passwordvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordValidatorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/passwordvalidator/PasswordValidatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.passwordvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
