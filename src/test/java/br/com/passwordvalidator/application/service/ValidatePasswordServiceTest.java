b2be07a7a541654aec7ebeedd11108dc99c80474
RodrigoLimaM <digo100200@gmail.com>
2022-03-08 21:21:55 -0300

    criando testes unitários

---
+++ src/test/java/br/com/passwordvalidator/application/service/ValidatePasswordServiceTest.java
@@ -0,0 +1,37 @@
+package br.com.passwordvalidator.application.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValidatePasswordServiceTest {
+
+    private final ValidatePasswordService validatePasswordService;
+
+
+    ValidatePasswordServiceTest() {
+        this.validatePasswordService = new ValidatePasswordService();
+    }
+
+    @Test
+    void shouldReturnTrue_whenPasswordIsValid() {
+        final boolean expected = true;
+
+        final boolean actual = validatePasswordService.validatePassword("Abcdefg@1");
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Abcdfg@1", "abcdefg@1", "ABCDEFG@1", "ABCDEFGH1", "ABCD EFGH1"})
+    void shouldReturnFalse_whenPasswordDoesntMeetTheRequirements(String password) {
+        final boolean expected = false;
+
+        final boolean actual = validatePasswordService.validatePassword(password);
+
+        assertEquals(expected, actual);
+    }
+
+}
